83fa2ebdba346ac4750b22254deb8ab2ebe67e17
Yashoman Wickramasinghe <12203888@students.koi.edu.au>
2024-12-11 13:12:54 +0000

    create an azure resource group using the C# Azure SDK

---
+++ package-dotnet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Storage;
+
+namespace AzureStorageDeployment
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Azure credentials using DefaultAzureCredential
+            var credential = new DefaultAzureCredential();
+
+             // Azure subscription ID
+            string subscriptionId = "02e52ae5-0d0d-453c-a3be-547122e9c912";
+
+            // Resource group and storage account details
+            string resourceGroupName = "my-sdk-yasho-rg";
+            
+            string storageAccountName = "helloworld0608"; // Must be globally unique
+
+            string location = "EastUS";
+
+            // Initialize the Azure Resource Manager Client
+            var client = new ArmClient(credential, subscriptionId);
+
+            SubscriptionResource subscription = await client.GetDefaultSubscriptionAsync();
+            ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
+            ResourceGroupData resourceGroupData = new ResourceGroupData(location);
+            ArmOperation<ResourceGroupResource> operation = await resourceGroups.CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
+
+            Console.WriteLine("Resource Group created successfully.");
+        }
+    }
+}
